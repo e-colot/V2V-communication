83bba292be777d8aced5a66530d9ccb622aa794d
E. Colot <emmeran.colot@gmail.com>
2025-04-18 11:37:13 +0200

    step 3.4

---
+++ steps_specific/step3_4.m
@@ -0,0 +1,48 @@
+% getting access to the project
+addpath(genpath('./..'));
+
+clear; close all; clc;
+
+cfg = config(); 
+
+distance = 1:0.1:1000; % distance from TX to the RX in meters
+LOSPower = zeros(length(distance), 1);
+MPCPower = zeros(length(distance), 1);
+
+%extend the walls to reach 1km
+cfg.obstacles = zeros(2, 2, 2);
+cfg.obstacles(1, :, 1) = [0, -cfg.environment_params.road_width/2];
+cfg.obstacles(2, :, 1) = [1100, -cfg.environment_params.road_width/2];
+
+cfg.obstacles(1, :, 2) = [0, cfg.environment_params.road_width/2];
+cfg.obstacles(2, :, 2) = [1100, cfg.environment_params.road_width/2];
+
+for i = 1:length(distance)
+    d = distance(i); % current distance
+    cfg.TX_pos = [50 + d; -cfg.environment_params.road_width/4];
+    cfg.RX_pos = [50; -cfg.environment_params.road_width/4];
+
+
+    rays = createRays(cfg);
+    rays.voltages = rayVoltage(rays, cfg); % calculate the voltages
+
+    LOSPower(i) = norm(rays.voltages(1)); 
+    MPCPower(i) = norm(sum(rays.voltages(2:end).^2));
+end
+
+% visualize(cfg, rays, 2);
+% xlim([30 170]);
+% ylim([-cfg.environment_params.road_width cfg.environment_params.road_width]);
+% view(114, 47);
+
+% Calculate the Rice factor (K) and plot it as a function of distance
+K = LOSPower ./ MPCPower;
+
+figure;
+plot(distance, 10*log10(K), 'LineWidth', 1.5);
+xlabel('Distance (m)');
+ylabel('Rice Factor (K) [dB]');
+title('Rice Factor (K) vs Distance');
+grid on;
+
+
