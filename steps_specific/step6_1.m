b0ef347a85e4ed51c523258c3676223cf54f108b
E. Colot <emmeran.colot@gmail.com>
2025-05-19 23:12:07 +0200

    sleep schedule is a scam invented by bed-sellers

--- steps_specific/step6_1.m
+++ steps_specific/step6_1.m
@@ -5,40 +5,47 @@ clear; close all; clc;
 
 cfg = config(); 
 
-cfg.bounce_limit = 3;
+cfg.bounce_limit = 6;
 cfg.local_area_len = 5;
 
-cfg.environment_params.road_length = 2000;
-
-%recompute obstacles
-cfg.obstacles = zeros(2, 2, 2);
-cfg.obstacles(1, :, 1) = [-1100, -cfg.environment_params.road_width/2];
-cfg.obstacles(2, :, 1) = [1100, -cfg.environment_params.road_width/2];
-
-cfg.obstacles(1, :, 2) = [-1100, cfg.environment_params.road_width/2];
-cfg.obstacles(2, :, 2) = [1100, cfg.environment_params.road_width/2];
+cfg.environment_params.road_length = 500;
+cfg.obstacles = createObstacles(cfg); % create obstacles
 
 
 x = (-cfg.environment_params.road_length+cfg.environment_params.local_area_len)/2:cfg.environment_params.local_area_len:(cfg.environment_params.road_length-cfg.environment_params.local_area_len)/2;
 y = (-cfg.environment_params.road_width+cfg.environment_params.local_area_len)/2:cfg.environment_params.local_area_len:(cfg.environment_params.road_width-cfg.environment_params.local_area_len)/2;
 
-cfg.TX_pos = [0; 0];
+cfg.TX_pos = [25; 0];
 
 avgPower = zeros(length(x), length(y)); % initialize power matrix
 distance = zeros(length(x), length(y)); % initialize distance matrix
+avgPowerPerp = zeros(length(y), length(x)); % initialize power matrix
+distancePerp = zeros(length(y), length(x)); % initialize distance matrix
 
 hWait = waitbar(0, 'Processing...'); % Initialize waitbar
 for xi = 1:length(x)
     for yi = 1:length(y)
-        cfg.RX_pos = [x(xi); y(yi)];
-        distance(xi, yi) = norm(cfg.RX_pos - cfg.TX_pos); % calculate distance
+        cfg_local = cfg; % Create a local copy of cfg for each worker
+        cfg_local.RX_pos = [x(xi); y(yi)];
+        distance(xi, yi) = norm(cfg_local.RX_pos - cfg_local.TX_pos); % calculate distance
 
-        rays = createRays(cfg);
-        rays.voltages = rayVoltage(rays, cfg); % calculate the voltages
+        rays = createRays(cfg_local);
+        rays.voltages = rayVoltage(rays, cfg_local); % calculate the voltages
 
         avgPower(xi, yi) = sum(abs(rays.voltages).^2)/(45*pi);
         if avgPower(xi, yi) == 0
             avgPower(xi, yi) = NaN; % avoid log(0)
+        end
+
+        cfg_local.RX_pos = [y(yi); x(xi)];
+        distancePerp(yi, xi) = norm(cfg_local.RX_pos - cfg_local.TX_pos); % calculate distance
+
+        rays = createRays(cfg_local);
+        rays.voltages = rayVoltage(rays, cfg_local); % calculate the voltages
+
+        avgPowerPerp(yi, xi) = sum(abs(rays.voltages).^2)/(45*pi);
+        if avgPowerPerp(yi, xi) == 0
+            avgPowerPerp(yi, xi) = NaN; % avoid log(0)
         end
     end
     waitbar(xi / length(x), hWait); % Update waitbar
@@ -48,48 +55,13 @@ close(hWait); % Close waitbar
 % Normalize the log of avgPower
 % multiplied by 1e3 to convert to mW and then to dBm
 logPower = 10*log10(avgPower*1e3);
-minVal = min(min(logPower));
-maxVal = max(max(logPower));
+logPowerPerp = 10*log10(avgPowerPerp*1e3);
+minVal = min(min(min(logPower)), min(min(logPowerPerp)));
+maxVal = max(max(max(logPower)), max(max(logPowerPerp)));
 normalizedPower = (logPower - minVal) / (maxVal - minVal);
+normalizedPowerPerp = (logPowerPerp - minVal) / (maxVal - minVal);
 
-
-% draw buildings
-    road_width = cfg.environment_params.road_width;          % Width of the road
-    road_length = cfg.environment_params.road_length;        % Length of the road
-    perp_road_length = cfg.environment_params.perpendicular_road_length; % Length of the perpendicular road
-    avg_building_depth = cfg.graphical_params.avg_building_depth;  % Depth of buildings
-    min_building_width = cfg.graphical_params.min_building_width;   % Minimum width of buildings
-    max_building_width = cfg.graphical_params.max_building_width;  % Maximum width of buildings
-    min_building_height = cfg.graphical_params.min_building_height; % Minimum height of buildings
-    max_building_height = cfg.graphical_params.max_building_height; % Maximum height of buildings
-    building_transparency = cfg.graphical_params.building_transparency;
-
-x_pos = -perp_road_length/2;
-while x_pos < perp_road_length/2
-    % Random building dimensions
-    building_width = min_building_width + (max_building_width - min_building_width) * rand;
-    building_heightT = min_building_height + (max_building_height - min_building_height) * rand;
-    building_heightB = min_building_height + (max_building_height - min_building_height) * rand;
-
-    % Top-side buildings
-    drawBuilding(x_pos, road_width/2, avg_building_depth, building_width, building_heightT, building_transparency);
-
-    % Bottom-side buildings
-    drawBuilding(x_pos, -road_width/2-avg_building_depth, avg_building_depth, building_width, building_heightB, building_transparency);
-
-    % Update x_pos for next building
-    x_pos = x_pos + building_width;
-end
-zlim([0, 70]); % Set z-axis limits
-ylim([-cfg.environment_params.road_width/2-avg_building_depth, cfg.environment_params.road_width/2+avg_building_depth]);
-xlim([-250, 250]);
-axis equal;
-    
-% Camera Settings
-view(-50, 15);
-camlight;
-lighting gouraud;
-
+visualize(cfg);
 hold on;
 
 
@@ -102,6 +74,12 @@ for xi = 1:length(x)
             colorIdx = max(1, min(size(cmap, 1), round(normalizedPower(xi, yi) * size(cmap, 1))));
             color = cmap(colorIdx, :); % Choose color based on normalized power
             plot3DCube(x(xi), y(yi), height, color, cfg.environment_params.local_area_len);
+        end
+        height = 50 * normalizedPowerPerp(yi, xi);
+        if (~isnan(height))
+            colorIdx = max(1, min(size(cmap, 1), round(normalizedPowerPerp(yi, xi) * size(cmap, 1))));
+            color = cmap(colorIdx, :); % Choose color based on normalized power
+            plot3DCube(y(yi), x(xi), height, color, cfg.environment_params.local_area_len);
         end
     end
 end
@@ -117,8 +95,8 @@ view(-40, 45);
 %% path loss model
 
 % Combine logPower and logPowerPerp into a single vector L
-L = -[logPower(:)] + 10*log10(cfg.transmit_params.TX_power*1e3); % remove TX power in dBm
-d = [distance(:)]; % combine distances
+L = -[logPower(:); logPowerPerp(:)] + 10*log10(cfg.transmit_params.TX_power*1e3); % remove TX power in dBm
+d = [distance(:); distancePerp(:)]; % combine distances
 
 validIdx = ~isnan(L); % find indices where L is not NaN
 L = L(validIdx); % keep only valid entries in L
@@ -195,39 +173,6 @@ function plot3DCube(x, y, height, color, width)
 
     % Plot the cube
     patch('Vertices', vertices, 'Faces', faces, 'FaceColor', color, 'EdgeColor', 'none');
-end
-
-% Function to Draw Buildings (3D Box)
-function drawBuilding(x, y, depth, width, height, building_transparency)
-    % x, y: Coordinates of the bottom corner of the building base with the lowest values along the x and y axes
-    % depth: Extends along the y-axis
-    % width: Extends along the x-axis
-    % height: Extends along the z-axis
-
-    % Define the vertices of the building (3D box)
-    vertices = [
-        x, y, 0;  % Bottom front-left (origin corner)
-        x + width, y, 0;  % Bottom front-right
-        x + width, y + depth, 0;  % Bottom back-right
-        x, y + depth, 0;  % Bottom back-left
-        x, y, height;  % Top front-left
-        x + width, y, height;  % Top front-right
-        x + width, y + depth, height;  % Top back-right
-        x, y + depth, height  % Top back-left
-    ];
-
-    % Define the faces of the building using the vertices
-    faces = [
-        1 2 6 5; % Front face
-        2 3 7 6; % Right face
-        3 4 8 7; % Back face
-        4 1 5 8; % Left face
-        5 6 7 8; % Top face
-        1 2 3 4; % Bottom face
-    ];
-
-    % Draw the building as a 3D patch
-    patch('Vertices', vertices, 'Faces', faces, 'FaceColor', [0.6 0.6 0.9], 'EdgeColor', 'k', 'FaceAlpha', building_transparency);
 end
 
 %% 3.7 range
